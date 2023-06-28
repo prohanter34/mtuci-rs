c4ae50ff31f62b68437d0bea7440b67f81ba98d6
karavaichek <prohanter34@gmail.com>
2023-06-28 17:43:24 +0300

    homework1

---
+++ homework1/src/main.rs
@@ -0,0 +1,25 @@
+trait Fizzbuzz {
+    fn fizzbuzz(&self);
+}
+
+impl Fizzbuzz for i32 {
+    fn fizzbuzz(&self) {
+        if self % 5 == 0 && self % 3 == 0 {
+            println!("FizzBuzz");
+        } else if self % 3 == 0 {
+            println!("Fizz");
+        } else if self % 5 == 0 {
+            println!("Buzz");
+        } else {
+            println!("{}", self);
+        }
+    }
+}
+
+
+fn main() {
+for i in 1..=100 {
+    i.fizzbuzz();
+}
+}
+
