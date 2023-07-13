ed73ee8296e5bbe8bd26b94750e3f5d67eb59a35
karavaichek <prohanter34@gmail.com>
2023-07-13 05:56:21 +0300

    myHashMap first

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,5 +1,14 @@
 //Вектор и хэшмап 
 
+use std::{rc::Rc, cell::RefCell};
+
 fn main() {
-    
+    let a = String::from("value");
+    let A = A {
+        vec: Rc::new(RefCell::new(a))
+    };
+}
+
+struct A {
+    vec: Rc<RefCell<String>>
 }
